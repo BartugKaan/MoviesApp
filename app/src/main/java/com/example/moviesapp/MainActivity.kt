30e187ff62ac6afa0c12b13ae56f80f5e4d9fee6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-01 16:16:18 +0300

    refactor: Implementation of MainScreenViewModel and it's UI

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,17 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviesapp.ui.screens.MainScreen
 import com.example.moviesapp.ui.theme.MoviesAppTheme
+import com.example.moviesapp.ui.viewmodel.MainScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val mainScreenViewModel: MainScreenViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MoviesAppTheme {
-
+                MainScreen(mainScreenViewModel = mainScreenViewModel)
             }
         }
     }
