1c6d9630935672d49f6940eeef9238565da3c806
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-01 14:54:55 +0300

    add: Dependency injection for Dao's and DataSources

---
+++ app/src/main/java/com/example/moviesapp/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.moviesapp.di
+
+import com.example.moviesapp.data.datasource.CartDataSource
+import com.example.moviesapp.data.datasource.MovieDataSource
+import com.example.moviesapp.retrofit.ApiUtils
+import com.example.moviesapp.retrofit.CartDao
+import com.example.moviesapp.retrofit.MovieDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+
+    @Provides
+    @Singleton
+    fun provideCartDataSource(cartDao: CartDao) : CartDataSource{
+        return  CartDataSource(cartDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDataSource(movieDao: MovieDao) : MovieDataSource{
+        return  MovieDataSource(movieDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieDao() : MovieDao{
+        return ApiUtils.getMovieDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCartDao() : CartDao{
+        return ApiUtils.getCartDao()
+    }
+}
