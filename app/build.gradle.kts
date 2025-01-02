27d66f6e10030e014aaaa12cf1ded489c09859cd
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-02 15:38:05 +0300

    add: Glide library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.6.1")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.6.1")
+    //Glide
+    implementation ("com.github.skydoves:landscapist-glide:1.4.4")
 }
