7f2a52c67362ef364bbe83c8a3fe4cb40c14ef68
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-07 20:58:06 +0300

    update: MainScreen UI update

--- app/src/main/java/com/example/moviesapp/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/moviesapp/ui/screens/MainScreen.kt
@@ -1,34 +1,36 @@
 package com.example.moviesapp.ui.screens
 
-import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.moviesapp.data.entity.Movie
 import com.example.moviesapp.ui.components.CarouselSlider
-import com.example.moviesapp.ui.components.CustomBottomAppBar
+import com.example.moviesapp.ui.components.CategoryButton
+import com.example.moviesapp.ui.components.MovieCard
 import com.example.moviesapp.ui.viewmodel.MainScreenViewModel
 import com.google.gson.Gson
 import com.skydoves.landscapist.glide.GlideImage
@@ -49,49 +51,48 @@ fun MainScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues),
+            .padding(paddingValues)
+            .background(MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         if (movieList.value.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {  }
             CircularProgressIndicator()
         } else {
-            Column(
-                horizontalAlignment = Alignment.Start,
-                modifier = Modifier.fillMaxWidth()
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
             ) {
-                CarouselSlider(movies = movieList.value, navController)
-                LazyVerticalGrid(
-                    modifier = Modifier.fillMaxSize(),
-                    columns = GridCells.Fixed(count = 2)
-                ) {
-                    items(
-                        //TODO:Custom Movie Cart needed
-                        count = movieList.value.count(),
-                        itemContent = {
-                            var movie = movieList.value[it]
-                            Log.e("Movie", "Movie name ${movie.name}")
-                            Card(modifier = Modifier.padding(all = 5.dp)) {
-                                Column(
-                                    modifier = Modifier.fillMaxWidth().clickable {
-                                        val movieJson = Gson().toJson(movie)
-                                        navController.navigate(route = "movieDetailScreen/$movieJson")
-                                    }
-                                ) {
-                                    GlideImage(imageModel = baseUrl + movie.image, modifier = Modifier.size(200.dp,300.dp))
-                                    Row(
-                                        modifier = Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.SpaceEvenly,
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        Text("${movie.name}", fontSize = 12.sp)
-                                    }
-                                }
-                            }
-                        }
+                item {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        item { CategoryButton("🎬All") { } }
+                        item { CategoryButton("⚔️ Action") { } }
+                        item { CategoryButton("🎭 Drama") { } }
+                        item { CategoryButton("👽 Sci-Fi") { } }
+                        item { CategoryButton("🐉 Fantastic") { } }
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    // Carousel Slider
+                    Text(
+                        text = "Top Rated Movies",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        color = MaterialTheme.colorScheme.primary
                     )
+                    CarouselSlider(movies = movieList.value, navController)
+
+                    Spacer(modifier = Modifier.height(4.dp))
                 }
             }
         }
     }
 }
+
