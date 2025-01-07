b73aaa62d1078a5cf1629ea2b8ff6dcc2b382626
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-07 20:57:38 +0300

    update: changed CustomBottomAppBar UI

--- app/src/main/java/com/example/moviesapp/ui/components/CustomBottomAppBar.kt
+++ app/src/main/java/com/example/moviesapp/ui/components/CustomBottomAppBar.kt
@@ -1,49 +1,42 @@
 package com.example.moviesapp.ui.components
 
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.moviesapp.R
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomBottomAppBar(navController: NavController, currentPageIndex: Int) {
-    NavigationBar {
+    NavigationBar(
+        modifier = Modifier.height(65.dp)
+    ) {
         NavigationBarItem(
             icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-            label = { Text("Home") },
             selected = currentPageIndex == 0,
             onClick = { navController.navigate("mainScreen") }
         )
         NavigationBarItem(
             icon = { Icon(Icons.Default.Favorite, contentDescription = "Favorites") },
-            label = { Text("Favorites") },
             selected = currentPageIndex == 1,
             onClick = { /* TODO */ }
         )
         NavigationBarItem(
             icon = { Icon(Icons.Default.ShoppingCart, contentDescription = "Cart") },
-            label = { Text("Cart") },
             selected = currentPageIndex == 2,
             onClick = { navController.navigate("cartScreen") }
         )
         NavigationBarItem(
             icon = { Icon(Icons.Default.Person, contentDescription = "Profile") },
-            label = { Text("Profile") },
             selected = currentPageIndex == 3,
             onClick = { /* TODO */ }
         )
