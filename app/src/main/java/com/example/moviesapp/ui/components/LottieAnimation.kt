5dc4ba3fbc561ae9b3396849e56eac4db0aac38f
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-10 18:25:14 +0300

    refactor: renamed and added smaller animation

--- app/src/main/java/com/example/moviesapp/ui/components/LottieAnimation.kt
+++ app/src/main/java/com/example/moviesapp/ui/components/LottieAnimation.kt
@@ -1,22 +1,25 @@
 package com.example.moviesapp.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
-fun LoadingAnimation(){
+fun LottieAnimation(url: String){
     //Composition for Lottie
     val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.Url("https://lottie.host/acb05c5e-96f0-4c9c-b816-d4b922aa4344/3J8DGFR3Iv.lottie")
+        spec = LottieCompositionSpec.Url(url)
     )
 
     Column(
@@ -25,6 +28,14 @@ fun LoadingAnimation(){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         LottieAnimation(composition, iterations = LottieConstants.IterateForever)
-        //CircularProgressIndicator()
     }
+}
+
+@Composable
+fun LottieAnimationSmall(url: String){
+    //Composition for Lottie
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.Url(url)
+    )
+        LottieAnimation(composition, iterations = LottieConstants.IterateForever, modifier = Modifier.size(height = 30.dp, width = 30.dp))
 }
