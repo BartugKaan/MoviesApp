21d96cade93a7e6d55f5615ef0aa1fa058b7c3bc
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-10 16:56:05 +0300

    fix: functions to suspend fun

--- app/src/main/java/com/example/moviesapp/datastore/AppPref.kt
+++ app/src/main/java/com/example/moviesapp/datastore/AppPref.kt
@@ -10,46 +10,43 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 
 class AppPref(var context: Context) {
-    val Context.ds : DataStore<Preferences> by preferencesDataStore("appInfo")
-
-    companion object{
+    companion object {
         val USERNAME_KEY = stringPreferencesKey("USERNAME")
         val ONBOARDING_KEY = booleanPreferencesKey("IS_ONBOARDING_SEEN")
+        private val Context.ds: DataStore<Preferences> by preferencesDataStore("appInfo")
     }
 
-    suspend fun saveUserName(userName:String){
+    suspend fun saveUserName(userName: String) {
         context.ds.edit {
             it[USERNAME_KEY] = userName
         }
     }
 
-    suspend fun getUserName():String{
+    suspend fun getUserName(): String {
         val p = context.ds.data.first()
         return p[USERNAME_KEY] ?: "Username could not found!"
     }
 
-    suspend fun deleteUserName(){
+    suspend fun deleteUserName() {
         context.ds.edit {
             it.remove(USERNAME_KEY)
         }
     }
 
-    suspend fun saveOnboardingPreferences(isSeen: Boolean){
+    suspend fun saveOnboardingPreferences(isSeen: Boolean) {
         context.ds.edit {
             it[ONBOARDING_KEY] = isSeen
         }
     }
 
-    suspend fun getOnboardingPreferences(): Boolean{
+    suspend fun getOnboardingPreferences(): Boolean {
         val p = context.ds.data.first()
         return p[ONBOARDING_KEY] ?: false
     }
 
-    suspend fun deleteOnboardingPreferences(){
+    suspend fun deleteOnboardingPreferences() {
         context.ds.edit {
             it.remove(ONBOARDING_KEY)
         }
     }
-
-
 }
